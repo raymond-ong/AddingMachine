36c9170fcc13def75341d02e83729dbff8d2bba6
Ong Raymond Aldwin <ongraymond@sgpoolz.com.sg>
2024-04-24 01:19:59 +0800

    Basic Func OK

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 window.elRefs = {};
 window.inputNumStr = "";
+window.arrNums = [];
 
 const onLoad = () => {
 	window.elRefs.inputScreen = document.getElementById('inputScreen');
+	window.elRefs.tblBody = document.getElementById('tblBody');
+	window.elRefs.totalTxt = document.getElementById('totalTxt');	
 }
 
-const onClear = () => {
+const onClearInput = () => {
 	window.inputNumStr = "";
 	window.elRefs.inputScreen.innerText = "";
 }
@@ -13,8 +16,13 @@ const onClear = () => {
 const onClickNum = (num) => {
 	window.inputNumStr += num;
 	window.elRefs.inputScreen.innerText = numberWithCommas(window.inputNumStr);
-
 }
+
+const onClickDot = () => {
+	window.inputNumStr += '.';
+	window.elRefs.inputScreen.innerText = numberWithCommas(window.inputNumStr);
+}
+
 
 // Utility for putting thousands/millions separator
 const numberWithCommas = (x) => {
@@ -22,5 +30,22 @@ const numberWithCommas = (x) => {
 }
 
 const onEnter = () => {
+	// Add new Number
+	var newNum = parseFloat(window.elRefs.inputScreen.innerText);
+	window.arrNums.push(newNum);	
+	addNewRow(newNum, window.arrNums.length);
+	onClearInput();
+
+	// Update Sum
+	let sumCalc = window.arrNums.reduce((currSum, x) => currSum + x, 0);
+	window.elRefs.totalTxt.innerHTML = sumCalc;
+}
+
+const addNewRow = (newNum, rowNum) => {	
+	let newRow =`<tr  class="tblRow"><td>${rowNum}</td><td class="amtCell">${newNum}</td></tr>`;
+	window.elRefs.tblBody.innerHTML += newRow;
+}
+
+const calculateSum = () => {
 
 }
